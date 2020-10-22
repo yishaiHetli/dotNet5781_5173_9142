7353eb148961a6bcadaea02d86eaf24c413573dd
yishai hetli <yishaihetli@gmail.com>
2020-10-22 14:43:04 +0300

    הוספתי פונקציה שמקבלת את שם המשתשתמש ומדפיסה את שמו

---
+++ dotNet5781_00_ 5173_9142/Program5173.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00__5173_9142
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5173();
+            Welcome9142();
+            Console.ReadKey();
+        }
+        
+        private static void Welcome5173()
+        {
+            Console.Write("Enter your name: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", a);
+        }
+
+        private static void Welcome9142() { }
+    }
+}
